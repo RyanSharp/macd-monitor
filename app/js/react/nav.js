69b8076293694e43b3d318111315fceb22423577
ryansharp <rtsharp90@gmail.com>
2017-03-22 04:31:02 +0000

    making app functional and good looking

--- app/js/react/nav.js
+++ app/js/react/nav.js
@@ -1,6 +1,12 @@
 class Nav extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            items: [],
+        };
+    }
+    handleHashChange() {
+        this.setState(parseHashAndQuery());
     }
     componentDidMount() {
         /*
@@ -8,31 +14,20 @@ class Nav extends React.Component {
          * for handling state changes using the hash of the URL
          * App state is handled on this react element.
          */
-        var a = document.createElement("a");
-        window.onhashchange = function(e) {
-            var hash, query, hashPath;
-            a.setAttribute("href", window.location.href);
-            hash = a.hash.split("/").slice(1).join("/").split("?");
-            query = {};
-            hash[1].split("&").map(function(pairs) {
-                pairs = pairs.split("=");
-                query[decodeURIComponent(pairs[0])] = decodeURIComponent(pairs[1]);
-            });
-            this.setState({
-                path: hash[0].split("/"),
-                params: query,
-            });
-        }
-        getSymbolsList().then(function(results) {this.setState({items: results})});
+        this.handleHashChange();
+        console.log(window.location.href);
+        window.onhashchange = this.handleHashChange.bind(this);
+        getSymbolsList().then(function(results) {this.setState({items: results})}.bind(this));
     }
     appContent() {
         var appContent = [];
         if (this.state && this.state.path && this.state.path[0] === "list") {
-            appContent.push(React.createElement(ListView, {items: this.state.items}));
+            appContent.push(React.createElement(ListView, {items: this.state.items, key: "main-list-view"}));
             if (this.state.params.ticker) {
-                appContent.push(React.createElement(StockProfile, {symbol: this.state.params.ticker}));
+                appContent.push(React.createElement(Modal, {content: React.createElement(StockProfile, {symbol: this.state.params.ticker})}));
             }
         }
+        return appContent;
     }
     render() {
         return React.createElement("div", {className: "app-container"}, this.appContent());
@@ -46,8 +41,8 @@ class ListView extends React.Component {
     }
     render() {
         return React.createElement("div", {className: "list-view"},
-            this.props.items.map(function(item) {
-                return React.createElement(ListItem, {ticker: item.ticker});
+            this.props.items.map(function(item, i) {
+                return React.createElement(ListItem, {ticker: item.ticker, key: "main-list-item-" + i});
             })
         );
     }
@@ -59,21 +54,26 @@ class ListItem extends React.Component {
         super(props);
     }
     openModal() {
-        var aTag = document.createElement("a");
-        aTag.setAttribute("href", window.location.href);
-        hash = a.hash.split("?");
-        query = {};
-        hash[1].split("&").map(function(pairs) {
-            pairs = pairs.split("=");
-            query[decodeURIComponent(paris[0])] = decodeURIComponent(pairs[1]);
-        });
-        query.ticker = this.props.ticker;
-        window.location.href = "#/" + hash + "?" + Object.keys(query).map(function(param) {return encodeURIComponent(param) + "=" + encodeURIComponent(query[param])}).join("&");
+        var urlInfo = parseHashAndQuery();
+        urlInfo.params.ticker = this.props.ticker;
+        window.location.href = generateUrl(urlInfo.path, urlInfo.params);
     }
     render() {
         return React.createElement("div", {
             className: "list-item",
-            onClick: this.openModal(),
+            onClick: this.openModal.bind(this),
         }, this.props.ticker);
+    }
+}
+
+
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return React.createElement("div", {className: "modal-backdrop"},
+            React.createElement("div", {className: "modal-container"}, this.props.content)
+        );
     }
 }
--- app/js/react/stock-profile.js
+++ app/js/react/stock-profile.js
@@ -3,19 +3,49 @@ class StockProfile extends React.Component {
         super(props);
     }
     drawPriceChart() {
-        var context;
+        var context, data;
         context = this.refs.priceChart.getContext("2d");
+        data = {labels: [], datasets: [{data: []}]};
+        this.state.archive.map(function(pt, i) {
+            data.labels.push(pt.date);
+            data.datasets[0].data.push(pt.price);
+        });
         this.priceChart = new Chart(context, {
             type: "line",
-            data: this.state.archive.map(function(pt) {return pt.price}),
+            data: data,
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                legend: {
+                    display: false
+                },
+                scales: {
+                    xAxes: [{display: false}]
+                }
+            }
         });
     }
     drawMacdChart() {
-        var context;
+        var context, data;
         context = this.refs.macdChart.getContext("2d");
+        data = {labels: [], datasets: [{data: []}]};
+        this.state.archive.map(function(pt, i) {
+            data.labels.push(pt.date);
+            data.datasets[0].data.push(pt.macd_ema9 ? pt.ema12 - pt.ema26 - pt.macd_ema9 : 0);
+        });
         this.macdChart = new Chart(context, {
             type: "bar",
-            data: this.state.archive.map(function(pt) {return pt.macd_ema9 ? pt.ema12 - pt.ema26 - pt.macd_ema9 : 0}),
+            data: data,
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                legend: {
+                    display: false
+                },
+                scales: {
+                    xAxes: [{display: false}]
+                }
+            }
         });
     }
     componentDidMount() {
@@ -26,10 +56,30 @@ class StockProfile extends React.Component {
             }.bind(this));
         }.bind(this))
     }
+    closeSelf() {
+        var urlInfo = parseHashAndQuery(), url;
+        console.log(urlInfo);
+        delete urlInfo.params.ticker;
+        url = generateUrl(urlInfo.path, urlInfo.query);
+        console.log(url);
+        window.location.href = url;
+    }
     render() {
         return React.createElement("div", {className: "stock-profile"},
-            React.createElement("canvas", {className: "stock-profile-chart", ref: "priceChart"}),
-            React.createElement("canvas", {className: "stock-profile-chart", ref: "macdChart"})
+            React.createElement("div", {className: "profile-header"}, 
+                React.createElement("div", {className: "title"}, "Analysis: " + this.props.symbol),
+                React.createElement("div", {className: "header-close"},
+                    React.createElement("i", {className: "material-icons", onClick: this.closeSelf.bind(this)}, "close")
+                )
+            ),
+            React.createElement("div", {className: "chart-container"},
+                React.createElement("div", {className: "stock-profile-chart"},
+                    React.createElement("canvas", {ref: "priceChart"})
+                ),
+                React.createElement("div", {className: "stock-profile-chart"},
+                    React.createElement("canvas", {ref: "macdChart"})
+                )
+            )
         );
     }
 }
